5ae263ae292fd4e989982d8f843410021b7448dc
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 22:50:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:Winter]
+  holiday_hash[:Winter].flatten
 
 end
 
