9d9b2c487f573256152174d3c403911d8a907739
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 22:24:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_supply =
+  holiday_supplies =
   {
     :winter => {
       :christmas => ["Lights", "Wreath"],
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     }
   }
   
-  holiday_hash[:summer][:fourth_of_july][1]
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
