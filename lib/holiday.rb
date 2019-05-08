5b98bf45cae325bdeaec3addcc0e65b99e755253
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 23:10:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,20 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  " + holiday + " " + supplies
     end
   end
-  
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+ 
 
 end
 
