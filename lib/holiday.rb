7d5f7538063debf613006bbf658a2600dc927136
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-08 22:26:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  
+   holiday_hash[:winter].each do |holiday, items|
+    items << supply
+    puts items
+    end
   
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
